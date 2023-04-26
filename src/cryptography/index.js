0612c729b44b69ced7113f4034700e23b48e58f3
Gabriel Freitas <gabriel.freitas@zenvia.com>
2023-04-25 21:09:57 -0300

    finalizando a criptografia

--- src/cryptography/index.js
+++ src/cryptography/index.js
@@ -1,41 +1,39 @@
-const { codes, reverseCodes } = require('./data')
+const { codes, reverseCodes } = require("./data");
 
 const criptoRule = (strCripto, cypher = true) => {
-  let valorFinal = ''
+  let valorFinal = "";
 
-  const aChaves = [77, 84, 79, 65, 73, 78, 67, 70, 82]
+  const aChaves = [77, 84, 79, 65, 73, 78, 67, 70, 82];
 
-  const arrayCaracteres = strCripto.split('')
+  const arrayCaracteres = strCripto.split("");
 
   arrayCaracteres.forEach((el, i) => {
-    let n = reverseCodes[el]
+    let n = reverseCodes[el];
 
     if (n > 31) {
-      n = n - 32
+      n = n - 32;
 
-      const valor = cypher ? 1 : -1
+      const valor = cypher ? 1 : -1;
 
-      n = n + aChaves[i % 9] * valor
+      n = n + aChaves[i % 9] * valor;
 
-      n = n % 224
+      n = n % 224;
 
       if (n < 0) {
-        n = 224 + n
+        n = 224 + n;
       }
 
-      n = n + 32
+      n = n + 32;
     }
 
-    valorFinal = valorFinal + codes[n]
-  })
+    valorFinal = valorFinal + codes[n];
+  });
 
-  console.log(valorFinal)
-
-  return valorFinal
-}
+  return valorFinal;
+};
 
 module.exports = {
   encrypt: (strCripto) => criptoRule(strCripto),
 
   decrypt: (strCripto) => criptoRule(strCripto, false),
-}
+};
